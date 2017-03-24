7440529134c3ab210fbbe656336285e77f75bb67
Levin Jian <jianzhirong@gmail.com>
2017-03-24 11:21:34 +0800

    visualzize estimated velocity postion, yaw angle

--- visualization_tool/analyzeresult.py
+++ visualization_tool/analyzeresult.py
@@ -21,9 +21,8 @@ class AnalyzeResult(object):
         
         cols = ['type','px_est','py_est','vel_abs','yaw_angle','yaw_rate','vx_est','vy_est','px_meas','py_meas','nis','px_gt','py_gt','vx_gt','vy_gt']
         df = pd.read_csv(kalman_input_file, sep= '\t',names = cols, header=None)
-        csv_file_name = os.path.dirname(kalman_input_file) + '/result_analysis/' + os.path.basename(kalman_input_file)[:-4] +'.csv'
-        df.to_csv(csv_file_name)
-        print(csv_file_name + ' saved')
+        self.csv_file_name = os.path.dirname(kalman_input_file) + '/result_analysis/' + os.path.basename(kalman_input_file)[:-4] +'.csv'
+      
         return df
     
     def disp_nis(self, df):
@@ -53,11 +52,41 @@ class AnalyzeResult(object):
         return
     
     
-    def visulize_data(self,df):
-        plt.scatter(df['px_gt'], df['py_gt'],marker='*', color='blue')
-        plt.scatter(df['px_meas'], df['py_meas'],marker='v',color='red')
-        plt.show()
+    def visulize_position(self,df):
+        gt = plt.plot(df['px_gt'], df['py_gt'],label='Ground Truth Position')
+        est = plt.plot(df['px_est'], df['py_est'],label='Estimated Position')
+        meas = plt.scatter(df['px_meas'], df['py_meas'],label='measurement')
+
+        plt.xlabel('Postiion estimation')
+        plt.ylabel('py')
+        plt.title('px')
+        plt.legend(loc='upper right')
+        
         return
+    
+    
+    def visualize_velocity(self,df):
+        v_gt = np.sqrt((df['vx_gt'].values **2 + df['vy_gt'].values **2))
+#         v_est = np.sqrt((df['vx_est'].values **2 + df['vy_est'].values **2))
+        v_est = df['vel_abs'].values
+        
+        
+        
+        plt.plot(v_gt, label='Ground Truth Velocity')
+        plt.plot(v_est, label='Estimated Velocity')
+        plt.legend(loc='upper right')
+        return
+    
+    def visualize_yaw_angle(self,df):
+        gt = np.arctan2(df['vy_gt'].values,  df['vx_gt'].values)
+
+        est = df['yaw_angle'].values
+        
+        plt.plot(gt, label='Ground Truth Yaw Angle')
+        plt.plot(est, label='Estimated Yaw Angle')
+        plt.legend(loc='upper right')
+        return
+    
     def run_data_1(self):
         print('####data sample 1###')
         kalman_input_file = r'../data/sample-laser-radar-measurement-data-1_output.txt'
@@ -67,16 +96,21 @@ class AnalyzeResult(object):
         return df
     def run_data_2(self):
         print('####data sample 2###')
-        kalman_input_file = r'../data/sample-laser-radar-measurement-data-2.txt'
+        kalman_input_file = r'../data/sample-laser-radar-measurement-data-2_output.txt'
         df = self.__load_input_data(kalman_input_file)
-        self.__cal_input_rmse(df)
+#         self.visulize_position(df)
+#         self.visualize_velocity(df)
+        self.visualize_yaw_angle(df)
+        df.to_csv(self.csv_file_name)
+        print(self.csv_file_name + ' saved')
         return df
     
    
         
     def run(self):
-        self.run_data_1()
-#         self.run_data_2()
+#         self.run_data_1()
+        self.run_data_2()
+        plt.show()
         
 #         self.visulize_data(df)
 
